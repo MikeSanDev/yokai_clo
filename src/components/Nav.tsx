6dfb1bfc651c10ce9ac31ce27234f052376a4b9e
MikeSanDev <mjs070594@gmail.com>
2024-09-03 16:51:57 -0500

    broken code

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,15 @@
-import { ReactNode } from "react";
+"use client"
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ComponentProps, ReactNode } from "react";
 
 export function Nav({ children }: { children: ReactNode }) {
     return <nav className="bg-primary text-primary-foreground flex justify-center px-4">{children}</nav>
+}
+
+
+export function NavLink(props: Omit<ComponentProps<typeof Link>. "className">)
+const pathname = usePathname()
+return <Link (...props} className = { cn() } /> 
 }
